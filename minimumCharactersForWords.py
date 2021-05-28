f9357c7b126c10acda8130c0e0104b4bda25afb1
Mahabubul Hasan <shourav6688@gmail.com>
2021-05-28 20:45:20 +0600

    minimumCharactersForWords updated

--- minimumCharactersForWords.py
+++ minimumCharactersForWords.py
@@ -1,78 +1,37 @@
 def minimumCharactersForWords(words):
-    global charactersFrequncies
-    maximumCharactersHashmap = {}
+    maximumCharacterHashmap = {}
     for i in words:
-        charactersFrequncies = countCharacterFrequency(i)
-        updateMaximumFrequencies(charactersFrequncies, maximumCharactersHashmap)
-    return makeArray(maximumCharactersHashmap)
+        characterCount = countCharacter(i)
+        compareBetweeenHashmap(characterCount, maximumCharacterHashmap)
+    return makeArray(maximumCharacterHashmap)
 
 
-def countCharacterFrequency(string):
-    hashTable = {}
+def countCharacter(string):
+    hashmap = {}
     for i in string:
-        if i in hashTable:
-            hashTable[i] += 1
+        if i in hashmap:
+            hashmap[i] += 1
         else:
-            hashTable[i] = 1
-    return hashTable
+            hashmap[i] = 1
+    return hashmap
 
 
-def updateMaximumFrequencies(frequencies, maximumFrequncies):
-    for i in frequencies:
-        frequency = frequencies[i]
-        if i in maximumFrequncies:
-            maximumFrequncies[i] = max(frequency, maximumFrequncies[i])
+def compareBetweeenHashmap(frequecies, maximumFrequencies):
+    for i in frequecies:
+        frequency = frequecies[i]
+        if i in maximumFrequencies:
+            maximumFrequencies[i] = max(frequency, maximumFrequencies[i])
         else:
-            maximumFrequncies[i] = frequency
+            maximumFrequencies[i] = frequency
 
 
-def makeArray(characterFrequency):
-    charcter = []
-    for i in characterFrequency:
-        frequency = characterFrequency[i]
+def makeArray(maximumFrequencies):
+    character = []
+    for i in maximumFrequencies:
+        frequency = maximumFrequencies[i]
         for _ in range(frequency):
-            charcter.append(i)
-    return charcter
+            character.append(i)
+    return character
 
 
 print(minimumCharactersForWords(["this", "that", "did", "deed", "them!", "a"]))
-# def minimumCharactersForWords(words):
-#     global characterFrequencies
-#     maximumCharacterFrequencies = {}
-#     for i in words:
-#         characterFrequencies = countCharacterFrequencies(i)
-#         print(characterFrequencies)
-#         updateMaximumFrequencies(characterFrequencies, maximumCharacterFrequencies)
-#
-#     return makeArray(maximumCharacterFrequencies)
-#
-#
-# def countCharacterFrequencies(string):
-#     characterFrequencies = {}
-#     for i in string:
-#         if i in characterFrequencies:
-#             characterFrequencies[i] += 1
-#         else:
-#             characterFrequencies[i] = 1
-#     return characterFrequencies
-#
-#
-#
-# def updateMaximumFrequencies(frequencies, maxFrequencies):
-#     for i in frequencies:
-#         frequency = frequencies[i]
-#         if i in maxFrequencies:
-#             maxFrequencies[i] = max(frequency, maxFrequencies[i])
-#         else:
-#             maxFrequencies[i] = frequency
-#
-#
-# def makeArray(characterFrequency):
-#     characters = []
-#     for i in characterFrequency:
-#         frequency = characterFrequency[i]
-#         for _ in range(frequency):
-#             characters.append(i)
-#     return characters
-#
-# print(minimumCharactersForWords(["this", "that", "did", "deed", "them!", "a"]))
