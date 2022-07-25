a0c7df5e02aeb7b2418648ce0ea627eac7700066
Mahabubul Hasan <mahabubul.hasan@upaybd.com>
2022-07-25 10:15:26 +0600

    week 9 run --phase 2

---
+++ find_first_and_last_posistion_in_an_array.py
@@ -0,0 +1,39 @@
+def searchStartIndex(arr, target):
+    index = -1
+    i, j = 0, len(arr) - 1
+    while i <= j:
+        mid = (i + j) // 2
+        if arr[mid] == target:
+            index = mid
+            j = mid - 1
+        elif arr[mid] > target:
+            j = mid - 1
+        else:
+            i = mid + 1
+    return index
+
+
+def searchEndIndex(arr, target):
+    index = -1
+    i, j = 0, len(arr) - 1
+    while i <= j:
+        mid = (i + j) // 2
+        if arr[mid] == target:
+            index = mid
+            i = mid + 1
+        elif arr[mid] > target:
+            j = mid - 1
+        else:
+            i = mid + 1
+    return index
+
+
+def searchRange(arr, target):
+    pos = [-1, -1]
+    pos[0] = searchStartIndex(arr, target)
+    pos[1] = searchEndIndex(arr, target)
+
+    return pos
+
+
+print(searchRange([5, 7, 7, 8, 8, 10], 8))
