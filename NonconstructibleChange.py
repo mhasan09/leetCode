cd6785b1d72815f619462651889e73898a54bfb3
Mahabubul Hasan <mahabubul.hasan@upaybd.com>
2022-07-26 13:19:14 +0600

    week 9 run --phase 4

--- NonconstructibleChange.py
+++ NonconstructibleChange.py
@@ -2,9 +2,10 @@ def nonConstructibleChange(coins):
     coins.sort()
     change = 0
     for i in range(0, len(coins)):
-        if coins[i] > change+1:
+        if coins[i] > change + 1:
             return change + 1
         change += coins[i]
-    return change+1
+    return change + 1
 
-print(nonConstructibleChange([5,6,1,1,2,3,4,9]))
+
+print(nonConstructibleChange([5, 6, 1, 1, 2, 3, 4, 9]))
