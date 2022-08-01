0f32feb25ed1d4232723e6b1a2f5f2ebf1c31821
Mahabubul Hasan <mahabubul.hasan@upaybd.com>
2022-08-01 17:30:00 +0600

    week 10 run --phase 2

---
+++ unique_paths.py
@@ -0,0 +1,11 @@
+def unique_paths(m, n):
+    row = [1] * n
+    for i in range(m - 1):
+        newRow = [1] * n
+        for j in range(n - 2, -1, -1):
+            newRow[j] = newRow[j + 1] + row[j]
+        row = newRow
+    return row[0]
+
+
+print(unique_paths(3, 2))
