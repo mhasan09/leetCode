d8c2fc0654c0cfab1a2fc838001593026cfa85c4
Mahabubul Hasan <mahabubul.hasan@upaybd.com>
2022-08-10 18:08:50 +0600

    week 10 run --phase 11 (unfinished)

--- valid_mountain_array.py
+++ valid_mountain_array.py
@@ -3,15 +3,15 @@ def validMountainArray(arr):
     q = len(arr) - 1
     peak = max(arr)
     peak_index = arr.index(peak)
-    if len(arr) == 1:
+    if len(arr) == 1 or len(arr) == 2:
         return False
     while p < peak_index:
-        if arr[p] < peak:
+        if arr[p] < peak and peak_index < len(arr) - 1:
             p += 1
         else:
             return False
     while peak_index < q:
-        if arr[peak_index + 1] < peak and arr[peak_index] < len(arr) - 1:
+        if arr[peak_index + 1] < peak and peak_index < len(arr) - 1:
             peak_index += 1
         else:
             return False
@@ -22,4 +22,6 @@ def validMountainArray(arr):
 # print(validMountainArray([3, 5, 5]))
 # print(validMountainArray([2, 1]))
 # print(validMountainArray([2]))
-print(validMountainArray([1, 3, 2]))
+# print(validMountainArray([1, 3, 2]))
+# print(validMountainArray([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
+print(validMountainArray([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]))
