f712dcbb1e86275ddc3d22bef4eb05bea732adac
Mahabubul Hasan <shourav6688@gmail.com>
2022-08-12 13:28:57 +0600

    week 10 run --phase 12

--- valid_mountain_array.py
+++ valid_mountain_array.py
@@ -1,17 +1,18 @@
 def validMountainArray(arr):
     p = 0
-    q = len(arr) - 1
+    q = 1
     peak = max(arr)
     peak_index = arr.index(peak)
-    if len(arr) == 1 or len(arr) == 2:
+    if len(arr) == 1 or len(arr) == 2 or peak_index == 0 or peak_index == len(arr) - 1:
         return False
     while p < peak_index:
-        if arr[p] < peak and peak_index < len(arr) - 1:
+        if arr[p] < arr[q]:
             p += 1
+            q += 1
         else:
             return False
-    while peak_index < q:
-        if arr[peak_index + 1] < peak and peak_index < len(arr) - 1:
+    while peak_index < len(arr) - 1:
+        if arr[peak_index] > arr[peak_index + 1]:
             peak_index += 1
         else:
             return False
@@ -24,4 +25,5 @@ def validMountainArray(arr):
 # print(validMountainArray([2]))
 # print(validMountainArray([1, 3, 2]))
 # print(validMountainArray([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
-print(validMountainArray([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]))
+# print(validMountainArray([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]))
+# print(validMountainArray([0, 1, 3, 2, 1, 2]))
