95e092b711592c55791ffaea549cd6460fba1087
Mahabubul Hasan <shourav6688@gmail.com>
2022-09-02 19:34:08 +0600

    week 13 run --phase 3

---
+++ string_to_integer_atoi.py
@@ -0,0 +1,33 @@
+def my_atoi(s):
+    if len(s) == 0:
+        return 0
+    i = 0
+    while i < len(s) and s[i] == ' ':
+        i += 1
+    if i == len(s):  # all are leading whitespace
+        return 0
+    if s[i] == '+':
+        sign = 1
+        i += 1
+    elif s[i] == '-':
+        sign = -1
+        i += 1
+    else:
+        sign = 1
+
+    val = 0
+    while i < len(s) and s[i].isdigit():
+        val = val * 10 + ord(s[i]) - ord('0')
+        i += 1
+
+    res = val * sign
+    if res < -2 ** 31:
+        return -2 ** 31
+    elif res > 2 ** 31 - 1:
+        return 2 ** 31 - 1
+    else:
+        return res
+
+
+print(my_atoi("   -42 bbll"))
+print(my_atoi("21474836460"))
