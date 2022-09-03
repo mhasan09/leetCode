13dfe7afc28118079ba79f6310b0a956d1299df4
Mahabubul Hasan <shourav6688@gmail.com>
2022-09-03 17:32:34 +0600

    week 13 run --phase 4

--- implement_strStr.py
+++ implement_strStr.py
@@ -1,10 +1,12 @@
 def str_str(haystack, needle):
     if len(needle) == 0:
         return 0
-    for i in haystack:
-        if needle in haystack:
+    for i in range(len(haystack)):
+        if haystack[i:i+len(needle)] == needle:
             return i
     return -1
+
+
 
 
 print(str_str("hello", "ll"))
