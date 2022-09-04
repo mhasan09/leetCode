efeb7a33ac55e34868883ebcbdb97fab2827690f
Mahabubul Hasan <mahabubul.hasan@upaybd.com>
2022-09-04 16:27:01 +0600

    week 14 run --phase 2

---
+++ linked_list_cycle.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class Solution:
+    def __init__(self):
+        self.head = None
+
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
