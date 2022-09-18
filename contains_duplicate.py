17dee96509972b6d488abd84a0fdfed50f510038
Mahabubul Hasan <mahabubul.hasan@upaybd.com>
2022-09-18 14:12:01 +0600

    week 15 run --phase 4

--- contains_duplicate.py
+++ contains_duplicate.py
@@ -1,20 +1,21 @@
+# def contains_duplicate(arr):
+#     data = {}
+#     for i in arr:
+#         if i in data:
+#             data[i] += 1
+#         else:
+#             data[i] = 1
+#     print(data)
+#
+#     for i in data:
+#         if data[i] > 1:
+#             return True
+#         else:
+#             continue
+#     return False
+
 def contains_duplicate(arr):
-    data = {}
-    for i in arr:
-        if i in data:
-            data[i] += 1
-        else:
-            data[i] = 1
-    print(data)
-
-    for i in data:
-        if data[i] > 1:
-            return True
-        else:
-            continue
-    return False
-
-
+    return len(arr) != len(set(arr))
 
 # print(contains_duplicate([1, 2, 3, 1]))
 # print(contains_duplicate([2, 14, 18, 22, 22]))
---
+++ maximum_subarray.py
@@ -0,0 +1,23 @@
+# def maxSubArray(arr):
+#     total_sum = arr[0]
+#     for i in range(len(arr)):
+#         for j in range(i + 1, len(arr)):
+#             current = 0
+#             for k in range(i, j):
+#                 current = current + arr[k]
+#                 total_sum = max(current, total_sum)
+#     return total_sum
+
+def maxSubArray(arr):
+    maximum = arr[0]
+    current = 0
+    for i in arr:
+        if current + i < i:
+            current = i
+        else:
+            current += i
+        maximum = max(current,maximum)
+    return maximum
+
+
+print(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
