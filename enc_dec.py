b917f48aa0283387a2bf088f85c4eb7185f65156
Mahabubul Hasan <mahabubul.hasan@upaybd.com>
2022-09-25 17:14:58 +0600

    week 15 run --phase 8

--- enc_dec.py
+++ enc_dec.py
@@ -1,24 +1,17 @@
+import ast
 from cryptography.fernet import Fernet
 
 
 def encrypter(key, plaintext):
     fernet = Fernet(key)
     enc_message = fernet.encrypt(plaintext.encode())
-    print('eeeeee',enc_message)
     return enc_message
 
 
 def decrypter(key, ciphertext):
-    c1 = "b'gAAAAABjMCbKdoHwKxecrT3vATPIeYkub49mmngwtsDfUKNE7Bm95eyDrevRv5KIkT4E88q7eClM_KV8WwOccn3ARjI3xceDzA=='"
-    c2 = c1[2:-1]
-    print("cccc2",c2)
-    print("cccc2",type(c2))
-    c3 = bytes(c2,'utf-8')
-    print('c3333333',c3)
-    print('c3333333',type(c3))
-
     fernet = Fernet(key)
-    dec_message = fernet.decrypt(c3).decode()
+    dec_message = fernet.decrypt(ast.literal_eval(ciphertext)).decode()
+    print(dec_message)
     return dec_message
 
 
@@ -29,17 +22,7 @@ def generatekey():
 
 if __name__ == "__main__":
     plaintext = "01674206688"
-
     key = generatekey()
-
     ciphertext = encrypter(key, plaintext)
-
-    print("")
-    print("Original string: ", plaintext)
-    print("")
-    print("Encrypted string: ", repr(ciphertext))
-    print("")
-    print("Key: ", repr(key))
-    print("")
-
-    decoded_message = decrypter(key, ciphertext)
+    decoded_message = decrypter(key, str(ciphertext))
+    print(decoded_message)
