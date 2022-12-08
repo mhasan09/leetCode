72069096516bf3cea2ae4b246e5dcf29277df74c
Mahabubul Hasan <mahabubul.hasan@upaybd.com>
2022-12-08 11:06:21 +0600

    week 20 run --phase 5

--- odd_even_linkedlist.py
+++ odd_even_linkedlist.py
@@ -8,5 +8,30 @@ class ListNode:
 
 
 class Solution:
-    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        pass
+    def odd_even_list(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        odd_head = ListNode()
+        odd_tail = odd_head
+
+        even_head = ListNode()
+        even_tail = even_head
+
+        i = 1
+
+        while head:
+            if i & 1:
+                odd_tail.next = head
+                odd_tail = odd_tail.next
+                head = head.next
+                odd_tail.next = None
+            else:
+                even_tail.next = head
+                even_tail = even_tail.next
+                head = head.next
+                even_tail.next = None
+            i += 1
+
+        odd_tail.next = even_head.next
+        return odd_head.next
+
+
+
