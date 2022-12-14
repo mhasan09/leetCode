1d5ac0f607f6e5e4cc22e775322517b7e477db2b
Mahabubul Hasan <mahabubul.hasan@upaybd.com>
2022-12-14 17:52:53 +0600

    week 20 run --phase 12

--- find_pivot_index.py
+++ find_pivot_index.py
@@ -1,7 +1,17 @@
 from typing import List
+import itertools
 
 
 class Solution:
     def pivotIndex(self, arr: List[int]) -> int:
-        left_sum = [0]
-        pass
+        value = itertools.accumulate(arr)
+        left_sum = [i for i in value]
+        left_sum.insert(0, 0)
+        right_sum = left_sum[::-1]
+        for i in range(len(left_sum)):
+            if left_sum[i] == right_sum[i]:
+                return i
+        return -1
+
+
+print(Solution().pivotIndex([1, 7, 3, 6, 5, 6]))
