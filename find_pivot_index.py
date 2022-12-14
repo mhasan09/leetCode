792a39cd8f7855c95a33d3355e0460fc6c722664
Mahabubul Hasan <shourav6688@gmail.com>
2022-12-14 23:49:33 +0600

    week 20 run --phase 13

--- find_pivot_index.py
+++ find_pivot_index.py
@@ -4,6 +4,21 @@ import itertools
 
 class Solution:
     def pivotIndex(self, arr: List[int]) -> int:
+        """
+        itertools solution
+        """
+        s1 = list(itertools.accumulate(arr))
+        s2 = list(itertools.accumulate(arr[::-1]))
+        s2 = s2[::-1]
+        for i in range(len(arr)):
+            if s1[i] == s2[i]:
+                return i
+        return -1
+
+    def pivotIndex_2(self, arr: List[int]) -> int:
+        """
+        sum solution
+        """
         left, right = 0, sum(arr)
         for index, num in enumerate(arr):
             right = right - num
