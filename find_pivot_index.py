f5f9c490c78e10bcd06d81b913af0029cfc074fb
Mahabubul Hasan <mahabubul.hasan@upaybd.com>
2022-12-14 12:21:41 +0600

    week 20 run --phase 10

---
+++ find_pivot_index.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        pass
---
+++ running_sum_of_1d_array.py
@@ -0,0 +1,11 @@
+import itertools
+from typing import List
+
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        result = itertools.accumulate(nums)
+        return [i for i in result]
+
+
+print(Solution().runningSum([1,2,3,4]))
