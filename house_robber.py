c9a87f0fcb62f48d0599db32061751f475280903
Mahabubul Hasan <mahabubul.hasan@upaybd.com>
2022-12-14 11:46:44 +0600

    week 20 run --phase 10

---
+++ house_robber.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+
+    def rob(self, arr):
+        if len(arr) == 0:
+            return 0
+
+        if len(arr) == 1:
+            return arr[0]
+
+        if len(arr) == 2:
+            max(arr[0], arr[1])
+
+        dp = [0] * len(arr)
+
+        dp[0], dp[1] = arr[0], max(arr[0], arr[1])
+
+        for i in range(2, len(arr)):
+            dp[i] = max(dp[i - 2] + arr[i], dp[i - 1])
+
+        return max(dp)
+
+
+print(Solution().rob([1, 2, 3, 1]))
