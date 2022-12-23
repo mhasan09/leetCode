b575b7588879ed0c312f37006a5cf7cfd78a59fa
Mahabubul Hasan <shourav6688@gmail.com>
2022-12-23 17:19:41 +0600

    week 21 run --phase 8

--- climbing_stairs.py
+++ climbing_stairs.py
@@ -1,22 +1,25 @@
 class Solution:
 
     def climbStairs(self, n: int) -> int:
-        data = [0 for i in range(50)]
+        data = [0 for i in range(45)]
 
-        def count_stairs(n):
-            if n <= 0:
-                return 0
-            if n == 1:
+        def stair_climb(x):
+            if x == 1:
                 return 1
-            elif n == 2:
+            elif x == 2:
                 return 2
 
-            if data[n] != 0:
-                return data[n]
-            value = count_stairs(n - 1) + count_stairs(n - 2)
-            data[n] = value
-            return value
-        return count_stairs(n)
+            if data[x] != 0:
+                return data[x]
+
+            result = stair_climb(x - 1) + stair_climb(x - 2)
+            data[x] = result
+            return result
+
+        return stair_climb(n)
 
 
-print(Solution().climbStairs(6))
+# print(Solution().climbStairs(6))
+# print(Solution().climbStairs(2))
+# print(Solution().climbStairs(3))
+print(Solution().climbStairs(38))
