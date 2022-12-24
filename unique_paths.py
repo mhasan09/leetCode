3d5579043aa7d427f068f4ccd431515f37741e0f
Mahabubul Hasan <shourav6688@gmail.com>
2022-12-24 17:05:22 +0600

    week 21 run --phase 10

--- unique_paths.py
+++ unique_paths.py
@@ -1,11 +1,9 @@
 def unique_paths(m, n):
-    row = [1] * n
-    for i in range(m - 1):
-        newRow = [1] * n
-        for j in range(n - 2, -1, -1):
-            newRow[j] = newRow[j + 1] + row[j]
-        row = newRow
-    return row[0]
+    dp = [[1 for _ in range(n)] for _ in range(m)]
+    for i in range(1,m):
+        for j in range(1,n):
+            dp[i][j] = dp[i-1][j] + dp[i][j-1]
+    return dp[m-1][n-1]
 
 
 print(unique_paths(3, 2))
