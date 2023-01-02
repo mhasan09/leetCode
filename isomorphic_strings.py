d2e8cbbeea69b4272620c6e911a4b35cadccdfb4
Mahabubul Hasan <mahabubul.hasan@upaybd.com>
2023-01-02 11:37:26 +0600

    week 22 run --phase 8

--- isomorphic_strings.py
+++ isomorphic_strings.py
@@ -1,20 +1,12 @@
-from collections import Counter
-
-
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        s1 = []
-        t1 = []
-        for i in s:
-            s1.append(s.index(i))
-        for i in t:
-            t1.append(t.index(i))
+        return list(map(s.find, s)) == list(map(t.find, t))
 
-        if s1 == t1:
-            return True
-        return False
+
+    '''
+        map takes 2 parameters -> (function, set/list/tuple)
+    
+    '''
 
 print(Solution().isIsomorphic("bbbaaaba", "aaabbbba"))
-# print(Solution().isIsomorphic("egg", "add"))
-
-
+print(Solution().isIsomorphic("egg", "add"))
