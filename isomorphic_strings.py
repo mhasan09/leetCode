c3d8b95fc61af03b95a3358836d0678ffb26f943
Mahabubul Hasan <mahabubul.hasan@upaybd.com>
2023-01-02 11:40:21 +0600

    week 22 run --phase 8

--- isomorphic_strings.py
+++ isomorphic_strings.py
@@ -2,12 +2,12 @@ class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         return list(map(s.find, s)) == list(map(t.find, t))
 
-
     '''
         map takes 2 parameters -> (function, set/list/tuple)
         find returns the first occurrence of the string item 
     
     '''
+
 
 print(Solution().isIsomorphic("bbbaaaba", "aaabbbba"))
 print(Solution().isIsomorphic("egg", "add"))
