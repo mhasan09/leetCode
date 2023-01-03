eb11b69a13a3d15b4184491b298ba518c3320877
Mahabubul Hasan <mahabubul.hasan@upaybd.com>
2023-01-03 13:30:16 +0600

    week 22 run --phase 10

--- delete_columns_to_make_sorted.py
+++ delete_columns_to_make_sorted.py
@@ -3,6 +3,15 @@ from typing import List
 
 class Solution:
     def minDeletionSize(self, strs: List[str]) -> int:
+        """
+            we're going to traverse the list by column, for example
+            ["cba", "daf", "ghi"] so it'll be c,d,g
+
+
+            so the outer loop will be "rrjk" -> len(rrjk) -> 4
+            and inner loop will be "r -> f -> g "
+
+        """
         count = 0
         for i in range(len(strs[0])):
             for j in range(1, len(strs)):
