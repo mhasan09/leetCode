9facb93612fd6a147f222366093eb1b98293fd52
Mahabubul Hasan <mahabubul.hasan@upaybd.com>
2023-01-05 12:22:19 +0600

    week 22 run --phase 9

--- delete_columns_to_make_sorted.py
+++ delete_columns_to_make_sorted.py
@@ -15,10 +15,11 @@ class Solution:
         count = 0
         for i in range(len(strs[0])):
             for j in range(1, len(strs)):
-                if strs[j][i] < strs[j-1][i]:
+                if strs[j][i] < strs[j - 1][i]:
                     count += 1
                     break
         return count
 
+
 # print(Solution().minDeletionSize(["cba", "daf", "ghi"]))
-print(Solution().minDeletionSize(["rrjk","furt","guzm"]))
+print(Solution().minDeletionSize(["rrjk", "furt", "guzm"]))
