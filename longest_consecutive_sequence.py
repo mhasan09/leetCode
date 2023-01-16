ca860a5a3082c63ffd62153b50074b8425a68c03
Mahabubul Hasan <mahabubul.hasan@upaybd.com>
2023-01-16 15:32:04 +0600

    week 23 run --phase 5

---
+++ longest_consecutive_sequence.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        elif len(nums) == 1:
+            return 1
+        else:
+            nums.sort()
+            count = 1
+            max_count = 1
+            for i in range(1, len(nums)):
+                if nums[i] == nums[i - 1]:
+                    continue
+                elif nums[i] == nums[i - 1] + 1:
+                    count += 1
+                else:
+                    count = 1
+                max_count = max(count, max_count)
+            return max_count
+
+
+# print(Solution().longestConsecutive([100, 4, 200, 1, 3, 2]))
+# print(Solution().longestConsecutive([0, 3, 7, 2, 5, 8, 4, 6, 0, 1]))
+# print(Solution().longestConsecutive([9, 1, 4, 7, 3, -1, 0, 5, 8, -1, 6]))
+print(Solution().longestConsecutive([1, 2, 0, 1]))
+# [0, 1, 1, 2]
+# [ 0,  1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# [-1, -1, 0, 1, 3, 4, 5, 6, 7, 8, 9]
