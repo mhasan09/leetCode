312f81dcee2d3245af9f03d5d4d1f2ea938d16fe
Mahabubul Hasan <mahabubul.hasan@upaybd.com>
2023-02-08 14:58:56 +0600

    week 25 run --phase 4

--- daily_temperatures.py
+++ daily_temperatures.py
@@ -23,6 +23,6 @@ class Solution:
         return result
 
 
-# print(Solution().dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]))
+print(Solution().dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]))
 # print(Solution().dailyTemperatures([30, 40, 50, 60]))
 print(Solution().dailyTemperatures([55, 38, 53, 81, 61, 93, 97, 32, 43, 78]))  # [3,1,1,2,1,1,0,1,1,0]
