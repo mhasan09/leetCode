cf62daf4275687376025f11622532dddeaff0ca3
Mahabubul Hasan <shourav6688@gmail.com>
2023-04-23 18:02:46 +0600

    week 28 run --phase 4

---
+++ longest_palindrome_by_concat_two_letter_words.py
@@ -0,0 +1,26 @@
+import collections
+from typing import List
+
+
+class Solution:
+    def longestPalindrome(self, words: List[str]) -> int:
+        wordMap = {}
+        res = 0
+        for word in words:
+            if wordMap.get(word[::-1], 0):
+                res += 4
+                wordMap[word[::-1]] -= 1
+            else:
+                wordMap[word] = 1 + wordMap.get(word, 0)
+
+        # in middle we can add only one unpaired word which itself is palindrome
+        for word in wordMap:
+            if word[0] == word[1] and wordMap[word] > 0:
+                res += 2
+                break
+        return res
+
+
+print(Solution().longestPalindrome(["lc", "cl", "gg"]))
+# print(Solution().longestPalindrome(["ab", "ty", "yt", "lc", "cl", "ab"]))
+# print(Solution().longestPalindrome(["cc", "ll", "xx"]))
