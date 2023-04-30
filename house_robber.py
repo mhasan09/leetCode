f326be6dfc84fd9f2ca02a668790ede4b84e4994
Mahabubul Hasan <mahabubul.hasan@upaybd.com>
2023-04-30 09:57:52 +0600

    week 29 run --phase 9

--- house_robber.py
+++ house_robber.py
@@ -11,16 +11,15 @@ class Solution:
             return arr[0]
 
         if len(arr) == 2:
-            max(arr[0], arr[1])
+            return max(arr[0], arr[1])
 
         dp = [0] * len(arr)
 
-        dp[0], dp[1] = arr[0], max(arr[0], arr[1])
+        dp[0] , dp[1] = arr[0], max(arr[0], arr[1])
 
         for i in range(2, len(arr)):
-            dp[i] = max(dp[i - 2] + arr[i], dp[i - 1])
+            dp[i] = max(dp[i-2] + arr[i], dp[i-1])
 
         return max(dp)
-
 
 print(Solution().rob([1, 2, 3, 1]))
