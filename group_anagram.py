c8686f930022448474424c77a3a94091c88579e8
Mahabubul Hasan <mahabubul.hasan@upaybd.com>
2023-05-29 13:14:46 +0600

    week 30 run --phase 3

--- group_anagram.py
+++ group_anagram.py
@@ -1,24 +1,5 @@
-from typing import List
-#
-#
-# class Solution:
-#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-#         if len(strs) == 0:
-#             return [[""]]
-#         ans = {}
-#
-#         for word in strs:
-#             temp = ''.join(sorted(word))
-#             if temp in ans:
-#                 ans[temp].append(word)
-#             else:
-#                 ans[temp] = [word]
-#         return list(ans.values())
-#
-#
-#
-# print(Solution().groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
 import collections
+from typing import List
 
 
 class Solution:
