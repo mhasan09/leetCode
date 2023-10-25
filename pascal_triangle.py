a8e46874feff706f8c3a55fd3af8da3910a37b2f
Mahabubul Hasan <mahabubul.hasan@upaybd.com>
2023-10-25 12:59:06 +0600

    week 39 run --phase 3

--- pascal_triangle.py
+++ pascal_triangle.py
@@ -3,16 +3,25 @@ from typing import List
 
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        l = [0] * numRows
-        for i in range(numRows):
+        output = []
+        for i in range(numRows+1):
+            temp_arr = []
+            output.append(temp_arr)
+            output[i] = [0] * i
+        output = output[1:]
 
-            l[i] = [0] * (i + 1)
-            l[i][0] = 1
-            l[i][i] = 1
+        row_len = len(output)
+        col_len = len(output[0])
 
-            for j in range(1, i):
-                l[i][j] = l[i-1][j-1] + l[i-1][j]
+        for row in range(row_len):
+            for col in range(col_len):
+                output[row][0] = 1
+                output[row][-1] = 1
 
-        return l
+        for row in range(2,len(output)):
+            for col in range(1,len(output[row])-1):
+                output[row][col] = output[row-1][col-1] + output[row-1][col]
+
+        return output
 
 print(Solution().generate(5))
