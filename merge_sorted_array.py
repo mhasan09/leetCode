c5ec2bbfa3075e0304bf574347d9e94c7b6f20a4
Mahabubul Hasan <shourav6688@gmail.com>
2023-11-06 13:04:34 +0600

    week 41 run --phase 2

--- merge_sorted_array.py
+++ merge_sorted_array.py
@@ -1,39 +1,15 @@
-# def merge_sorted_array(arr1, m, arr2, n):
-#     last = m + n - 1
-#     while m > 0 and n > 0:
-#         if arr1[m-1] < arr2[n-1]:
-#             arr1[last] = arr2[n-1]
-#             n -= 1
-#         else:
-#             arr1[last] = arr1[m-1]
-#             m -= 1
-#         last -= 1
-#
-#     while n > 0:
-#         arr1[last] = arr2[n-1]
-#         n -= 1
-#         last -= 1
-#
-#
-#
-# print(merge_sorted_array([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3))
-# print(merge_sorted_array([0], 0, [1], 1))
-# print(merge_sorted_array([1], 1, [], 0))
-
-
 def merge_sorted_array(arr1, m, arr2, n):
-    while m > 0 and n > 0:
-        if arr1[m - 1] < arr2[n - 1]:
-            arr1[m + n - 1] = arr2[n - 1]
-            m -= 1
+    a, b, write_index = m-1, n-1, m+n-1
+    while b >= 0:
+        if a>=0 and arr1[a] > arr2[b]:
+            arr1[write_index] = arr1[a]
+            a -= 1
         else:
-            arr1[m + n - 1] = arr1[m - 1]
-            n -= 1
-    while n > 0:
-        arr1[m + n - 1] = arr2[n - 1]
-        n -= 1
+            arr1[write_index] = arr2[b]
+            b -= 1
+        write_index -= 1
 
 
 print(merge_sorted_array([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3))
-print(merge_sorted_array([0], 0, [1], 1))
-print(merge_sorted_array([1], 1, [], 0))
+# print(merge_sorted_array([0], 0, [1], 1))
+# print(merge_sorted_array([1], 1, [], 0))
