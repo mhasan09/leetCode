f7517ae5eda8f7217a04912ba7b0de3e0ff31f41
Mahabubul Hasan <shourav6688@gmail.com>
2023-11-29 00:01:01 +0600

    week 42 run --phase 2

--- unique_paths.py
+++ unique_paths.py
@@ -1,9 +1,27 @@
-def unique_paths(m, n):
-    dp = [[1 for _ in range(n)] for _ in range(m)]
-    for i in range(1,m):
-        for j in range(1,n):
-            dp[i][j] = dp[i-1][j] + dp[i][j-1]
-    return dp[m-1][n-1]
+class Solution:
+    def calculate_paths(self, i, j, m, n, data):
+        # check whether we're in the last cell
+        if i == m - 1 and j == n - 1:
+            return 1
+
+        # check whether it's already computed or not
+        if data[i][j] != -1:
+            return data[i][j]
+
+        right_path, down_path = 0, 0
+        if i < m - 1:
+            right_path = self.calculate_paths(i+1, j, m, n, data)
+
+        if j < n - 1:
+            down_path = self.calculate_paths(i, j+1, m, n, data)
+
+        data[i][j] = right_path + down_path
+        return data[i][j]
 
 
-print(unique_paths(3, 2))
+    def uniquePaths(self, m: int, n: int) -> int:
+        data = [[-1 for _ in range(n)] for _ in range(m)]
+        return self.calculate_paths(0, 0, m, n, data)
+
+
+print(Solution().uniquePaths(3, 2))
