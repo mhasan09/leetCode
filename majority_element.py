9d55294e3b0038b197c0cefd5704c8f5350dc8c0
Mahabubul Hasan <shourav6688@gmail.com>
2023-12-09 10:53:16 +0600

    week 44 run --phase 1

--- majority_element.py
+++ majority_element.py
@@ -1,19 +1,28 @@
 class Solution:
     def majorityElement(self, nums):
         """ using moore's voting algorithm """
+        n = len(nums)
         count = 0
-        candidate = 0
+        candidate = None
 
-        for num in nums:
+        for i in range(n):
             if count == 0:
-                candidate = num
-
-            if num == candidate:
+                count = 1
+                candidate = nums[i]
+            elif candidate == nums[i]:
                 count += 1
             else:
                 count -= 1
 
-        return candidate
+        # checking if the stored element is the majority element
+        count_1 = 0
+        for i in range(n):
+            if nums[i] == candidate:
+                count_1 += 1
+
+        if count_1 > (n / 2):
+            return candidate
+        return -1
 
 
-print(Solution().majorityElement([2, 2, 3, 3, 4, 4, 1, 1, 5]))
+print(Solution().majorityElement([2, 2, 1, 1, 4, 4, 1, 1, 5]))
