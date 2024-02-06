dc00d28617338605538e2fb85e5d2a71a7f293d1
Mahabubul Hasan <mahabubul.hasan@upaybd.com>
2024-02-06 18:16:12 +0600

    week 49 run --phase 1

--- fractional_knapsack.py
+++ fractional_knapsack.py
@@ -1,5 +1,20 @@
 class Solution:
-    def fractionalknapsack(self, weight,value,n):
-        pass
+    def fractionalknapsack(self, weight, value, n, w):
+        weigh_val_index = [value // weight for value, weight in list(zip(value, weight))]
+        data = sorted(list(zip(value, weight, weigh_val_index)), key=lambda x: x[2], reverse=True)
+        current_weight = 0
+        current_val = 0
+        remaining_weight = w
+        for i in data:
+            if remaining_weight < i[1]:
+                current_val += remaining_weight * i[2]
+            else:
+                current_weight += i[1]
+                current_val += i[0]
+                remaining_weight = w - current_weight
 
-print(Solution().fractionalknapsack([],[],3))
+        return current_val
+
+
+print(Solution().fractionalknapsack([10, 20, 30], [60, 100, 120], 3, 50))
+print(Solution().fractionalknapsack([10, 20], [60, 100], 2, 50))
