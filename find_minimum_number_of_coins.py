290ead13383a5244fd76151addd1834cb824de78
Mahabubul Hasan <mahabubul.hasan@upaybd.com>
2024-02-07 17:08:14 +0600

    week 49 run --phase 2

---
+++ find_minimum_number_of_coins.py
@@ -0,0 +1,9 @@
+class Solution:
+    def find_minimum_number_of_coins(self, note):
+        currency = [1, 2, 5, 10, 20, 50, 100, 500, 1000]
+        answer = []
+        pass
+
+print(Solution().find_minimum_number_of_coins(70))
+print(Solution().find_minimum_number_of_coins(121))
+print(Solution().find_minimum_number_of_coins(49))
