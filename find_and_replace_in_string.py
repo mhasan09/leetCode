a2ae5174d87af9b3e10381b4325a07be25a1700e
Mahabubul Hasan <shourav6688@gmail.com>
2024-02-19 00:42:43 +0600

    week 50 run --phase 1

--- find_and_replace_in_string.py
+++ find_and_replace_in_string.py
@@ -3,18 +3,30 @@ from typing import List
 
 class Solution:
     def findReplaceString(self, s: str, indices: List[int], sources: List[str], targets: List[str]) -> str:
-        lookup = {i: (src, target) for i, src, target in zip(indices, sources, targets)}
-        i, result = 0, ""
-        while i < len(s):
-            if i in lookup and s[i:].startswith(lookup[i][0]):
-                result += lookup[i][1]
-                i += len(lookup[i][0])
-            else:
-                result += s[i]
-                i += 1
-        return result
+
+        # Copy string in a traget string
+        r = str(s[:])
+
+        # This variable keep track of how much indexes is shifted
+        # in resultant string w.r.t original string
+        shifted = 0
+
+        for idx, source, target in sorted(zip(indices, sources, targets)):
+
+            # If source is present in string
+            if s[idx:idx + len(source)] == source:
+                # Recalibrate index for resultant string
+                idx = idx + shifted
+
+                # Add target to resultant string
+                r = r[:idx] + target + r[idx + len(source):]
+
+                # Update the shifted variable, it can be negative or positive
+                shifted += len(target) - len(source)
+
+        return r
 
 
-# print(Solution().findReplaceString("abcd", [0, 2], ["a", "cd"], ["eee", "ffff"]))  # expected:"eeebffff"
+print(Solution().findReplaceString("abcd", [0, 2], ["a", "cd"], ["eee", "ffff"]))  # expected:"eeebffff"
 # print(Solution().findReplaceString("abcd", [0, 2], ["ab", "ec"], ["eee", "ffff"]))
-print(Solution().findReplaceString("vmokgggqzp", [3, 5, 1], ["kg", "ggq", "mo"], ["s", "so", "bfr"]))
+# print(Solution().findReplaceString("vmokgggqzp", [3, 5, 1], ["kg", "ggq", "mo"], ["s", "so", "bfr"]))
