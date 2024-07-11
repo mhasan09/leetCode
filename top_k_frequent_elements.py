6022a90d8445cfbff0c6ba8bc80468b1a0240769
Mahabubul Hasan <shourav6688@gmail.com>
2024-07-11 21:03:18 +0600

    week 62 run --phase 3

--- top_k_frequent_elements.py
+++ top_k_frequent_elements.py
@@ -1,3 +1,4 @@
+import heapq
 from typing import List
 from collections import Counter
 
@@ -7,7 +8,18 @@ class Solution:
         freq = list(sorted(Counter(nums).items(), key=lambda x:x[1], reverse=True))
         return [freq[i][0] for i in range(k)]
 
+    def topKFrequent_using_heap(self, nums: List[int], k: int) -> List[int]:
+        heap = []
+        data = Counter(nums)
+        for key,val in data.items():
+            if len(heap) < k:
+                heapq.heappush(heap, (val,key))
+            else:
+                heapq.heappushpop(heap, (val, key))
+
+        return [h[1] for h in heap]
 
 
 # print(Solution().topKFrequent([1, 1, 1, 2, 2, 3], 2))
-print(Solution().topKFrequent([3, 0, 1, 0], 1))
+# print(Solution().topKFrequent([3, 0, 1, 0], 1))
+print(Solution().topKFrequent_using_heap([1,1,2,2,2,3,3,3,3], 2))
