784926f967a92f76227319b467e6d27edeae07c8
Mahabubul Hasan <shourav6688@gmail.com>
2024-08-12 11:29:43 +0600

    week 65 run --phase 1

--- longest_common_subsequence.py
+++ longest_common_subsequence.py
@@ -1,16 +1,15 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        matrix = [[0 for _ in range(len(text2)+1)] for _ in range(len(text1)+1)]
+        data = [[0 for _ in range(len(text2) + 1)] for _ in range(len(text1) + 1)]
         longest = 0
         for i in range(0, len(text1)):
             for j in range(0, len(text2)):
                 if text1[i] == text2[j]:
-                    matrix[i][j] = 1 + matrix[i-1][j-1]
+                    data[i][j] = 1 + data[i-1][j-1]
                 else:
-                    matrix[i][j] = max(matrix[i-1][j],matrix[i][j-1])
-                longest = max(longest,matrix[i][j])
+                    data[i][j] = max(data[i-1][j], data[i][j-1])
+                longest = max(longest, data[i][j])
         return longest
-
 
 
 print(Solution().longestCommonSubsequence("stone", "longest"))
