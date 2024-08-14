1000f9f787b78732cae7bf2999fd8971cdb65d29
Mahabubul Hasan <shourav6688@gmail.com>
2024-08-14 22:20:12 +0600

    week 65 run --phase 3

--- house_robber.py
+++ house_robber.py
@@ -2,24 +2,23 @@ from typing import List
 
 
 class Solution:
-
     def rob(self, arr):
         if len(arr) == 0:
             return 0
-
         if len(arr) == 1:
             return arr[0]
 
-        if len(arr) == 2:
-            return max(arr[0], arr[1])
-
-        dp = [0] * len(arr)
-
-        dp[0] , dp[1] = arr[0], max(arr[0], arr[1])
+        prev2 = arr[0]
+        prev1 = max(arr[0], arr[1])
 
         for i in range(2, len(arr)):
-            dp[i] = max(dp[i-2] + arr[i], dp[i-1])
+            current = max(arr[i] + prev2, prev1)
+            prev2 = prev1
+            prev1 = current
 
-        return max(dp)
+
+        return prev1
+
 
 print(Solution().rob([1, 2, 3, 1]))
+print(Solution().rob([13, 9, 1, 63, 3, 4]))
