795aec4f248c24888b0e4448bce03d464dcea48a
Mahabubul Hasan <shourav6688@gmail.com>
2024-08-15 21:53:26 +0600

    week 65 run --phase 3

--- climbing_stairs.py
+++ climbing_stairs.py
@@ -1,21 +1,16 @@
 class Solution:
-
     def climbStairs(self, n: int) -> int:
-        dp = [-1] * (n+1)
-        return self.helper(n, dp)
-
-    def helper(self,n, dp):
-        if n < 0:
-            return 0
-        if n == 0:
+        if n == 1:
             return 1
-        if dp[n] != -1:
-            return dp[n]
-
-        dp[n] = self.helper(n-1, dp) + self.helper(n-2, dp)
+        if n == 2:
+            return 2
+        dp = [0] * (n+1)
+        dp[0] = 0
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3, n+1):
+            dp[i] = dp[i-2] + dp[i-1]
         return dp[n]
 
-# print(Solution().climbStairs(6))
-# print(Solution().climbStairs(2))
-# print(Solution().climbStairs(3))
-print(Solution().climbStairs(3))
+
+print(Solution().climbStairs(4))
